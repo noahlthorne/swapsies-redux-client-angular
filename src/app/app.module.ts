d320b03a29fabcb41e956148a7656dc6c72707ba
noahlthorne <noahlthorne@gmail.com>
2021-06-06 13:14:30 +0900

    added http client to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GamesComponent } from './components/games/games.component';
+import { GameComponent } from './components/game/game.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GamesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, GamesComponent, GameComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
