f50182a7fe24d15368daf61e734793fac703034a
noahlthorne <noahlthorne@gmail.com>
2021-06-06 11:43:59 +0900

    change title and fix tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'swapsies-redux-client-angular';
+  title = 'swapsies-redux-client';
 }
