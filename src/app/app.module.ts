190e6e16c6ea3d5a1f2079b554c1169c44b84b5f
noahlthorne <noahlthorne@gmail.com>
2021-06-07 09:37:29 +0900

    add listing create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GamesContainerComponent } from './components/containers/games-container/games-container.component';
 import { GameComponent } from './components/game/game.component';
+import { ListingCreateComponent } from './components/listing-create/listing-create.component';
 
 @NgModule({
-  declarations: [AppComponent, GamesContainerComponent, GameComponent],
+  declarations: [AppComponent, GamesContainerComponent, GameComponent, ListingCreateComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/listing-create/listing-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-create',
+  templateUrl: './listing-create.component.html',
+  styleUrls: ['./listing-create.component.scss']
+})
+export class ListingCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
