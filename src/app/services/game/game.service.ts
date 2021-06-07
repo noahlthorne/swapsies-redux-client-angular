6ae62216dafe4971cd53f3ce38f65a019b7e53e4
noahlthorne <noahlthorne@gmail.com>
2021-06-07 20:13:35 +0900

    move game service

---
+++ src/app/services/game/game.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Game } from '../../models/Game.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GameService {
+  gamesUrl: string = 'http://localhost:5000/api/games';
+  constructor(private http: HttpClient) {}
+
+  getGames = (): Observable<Game[]> => {
+    return this.http.get<Game[]>(this.gamesUrl);
+  };
+}
