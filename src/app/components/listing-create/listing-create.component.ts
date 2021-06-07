f8bbdb2912740527a3b1249c45f347448832eed6
noahlthorne <noahlthorne@gmail.com>
2021-06-07 19:25:15 +0900

    add listings create form

--- src/app/components/listing-create/listing-create.component.ts
+++ src/app/components/listing-create/listing-create.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Listing } from '../../models/Listing.model';
 
 @Component({
   selector: 'app-listing-create',
   templateUrl: './listing-create.component.html',
-  styleUrls: ['./listing-create.component.scss']
+  styleUrls: ['./listing-create.component.scss'],
 })
 export class ListingCreateComponent implements OnInit {
+  enteredValue: string = '';
+  newListing: Listing;
+  @Output() listingCreated = new EventEmitter<Listing>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  onAddListing = (form: NgForm) => {
+    console.log(form);
+    this.newListing = {
+      user: '123',
+      game: 'abc',
+      condition: 'new',
+    };
+    this.listingCreated.emit(this.newListing);
+  };
 }
