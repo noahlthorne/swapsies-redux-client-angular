884cba4a2762a1bbf6d655e3d54b43d308ff4346
noahlthorne <noahlthorne@gmail.com>
2021-06-07 19:22:29 +0900

    add onListingAdded function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Listing } from './models/Listing.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'swapsies-redux-client';
+  listings: Listing[] = [];
+
+  onListingAdded = async (listing: Listing) => {
+    this.listings.push(listing);
+  };
 }
