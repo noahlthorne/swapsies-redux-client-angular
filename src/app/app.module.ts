78016826cb8e39040f22b63d3e5e42d49cad50fc
noahlthorne <noahlthorne@gmail.com>
2021-06-07 10:41:31 +0900

    add matInputModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GamesContainerComponent } from './components/containers/games-container/games-container.component';
 import { GameComponent } from './components/game/game.component';
 import { ListingCreateComponent } from './components/listing-create/listing-create.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     GameComponent,
     ListingCreateComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
