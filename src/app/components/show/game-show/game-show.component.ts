5154395dc1bd2e4c43fedc2082d38f5a326e74e9
noahlthorne <noahlthorne@gmail.com>
2021-06-08 23:08:45 +0900

    add game show component

--- src/app/components/show/game-show/game-show.component.ts
+++ src/app/components/show/game-show/game-show.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models/Game.model';
+import { GameService } from 'src/app/services/game/game.service';
 
 @Component({
   selector: 'app-game-show',
   templateUrl: './game-show.component.html',
-  styleUrls: ['./game-show.component.scss']
+  styleUrls: ['./game-show.component.scss'],
 })
-export class GameShowComponent implements OnInit {
+export class GameShowComponent implements OnInit, OnDestroy {
+  game: Game;
+  private gameSub: Subscription;
+  private gameId: string | null;
 
-  constructor() { }
+  constructor(private gameService: GameService, public route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      this.gameId = paramMap.get('gameId');
+    });
+    this.gameSub = this.gameService.getGame(this.gameId).subscribe((game) => {
+      this.game = game;
+    });
   }
 
+  ngOnDestroy() {
+    this.gameSub.unsubscribe();
+  }
 }
--- src/app/services/game/game.service.ts
+++ src/app/services/game/game.service.ts
@@ -2,15 +2,25 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Game } from '../../models/Game.model';
+import { Listing } from 'src/app/models/Listing.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameService {
+  games: Game[] = [];
   gamesUrl: string = 'http://localhost:5000/api/games';
   constructor(private http: HttpClient) {}
 
   getGames = (): Observable<Game[]> => {
     return this.http.get<Game[]>(this.gamesUrl);
+  };
+
+  getGame = (gameId: string | null): Observable<Game> => {
+    return this.http.get<Game>(`${this.gamesUrl}/${gameId}`);
+  };
+
+  getGameListings = (gameId: string | null): Observable<Listing> => {
+    return this.http.get<Listing>(`${this.gamesUrl}/${gameId}`);
   };
 }
