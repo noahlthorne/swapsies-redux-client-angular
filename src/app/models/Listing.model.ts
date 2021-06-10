52896c47d9e09df41a00d9749eaadc3fe55831db
noahlthorne <noahlthorne@gmail.com>
2021-06-10 15:05:10 +0900

    fix listing model

--- src/app/models/Listing.model.ts
+++ src/app/models/Listing.model.ts
@@ -1,7 +1,8 @@
+import { Game } from './Game.model';
 export interface Listing {
   id: string;
-  user: string;
-  game: string;
+  user: any;
+  game: Game;
   condition: string;
   status?: string;
 }
