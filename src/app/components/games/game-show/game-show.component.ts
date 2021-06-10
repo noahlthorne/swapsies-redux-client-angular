a85ec07d12f18c93609b0673c7275bc856670500
noahlthorne <noahlthorne@gmail.com>
2021-06-10 16:12:52 +0900

    add animation to listing create

--- src/app/components/games/game-show/game-show.component.ts
+++ src/app/components/games/game-show/game-show.component.ts
@@ -3,11 +3,36 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Game } from 'src/app/models/Game.model';
 import { GameService } from 'src/app/services/game/game.service';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-game-show',
   templateUrl: './game-show.component.html',
   styleUrls: ['./game-show.component.scss'],
+  animations: [
+    trigger('popOverState', [
+      state(
+        'show',
+        style({
+          opacity: 1,
+        })
+      ),
+      state(
+        'hide',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('show => hide', animate('600ms ease-out')),
+      transition('hide => show', animate('1000ms ease-in')),
+    ]),
+  ],
 })
 export class GameShowComponent implements OnInit, OnDestroy {
   game: Game;
@@ -21,7 +46,9 @@ export class GameShowComponent implements OnInit, OnDestroy {
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('gameId')) {
         this.gameId = paramMap.get('gameId')!;
-        this.game = this.gameService.getGame(this.gameId)!;
+        this.gameService.getGame(this.gameId).subscribe((game) => {
+          this.game = game;
+        });
       }
     });
   }
@@ -34,5 +61,9 @@ export class GameShowComponent implements OnInit, OnDestroy {
 
   onClick() {
     this.display = !this.display;
+  }
+
+  get stateName() {
+    return this.display ? 'show' : 'hide';
   }
 }
