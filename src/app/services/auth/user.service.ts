f62da7546ac87b902ece28e08d6b5993a73aa5ae
noahlthorne <noahlthorne@gmail.com>
2021-06-16 17:41:13 +0900

    add token data and expiration date to local storage

--- src/app/services/auth/user.service.ts
+++ src/app/services/auth/user.service.ts
@@ -52,6 +52,11 @@ export class UserService {
           }, expiresInDuration * 1000);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
+          const now = new Date();
+          const expirationDate = new Date(
+            now.getTime() + expiresInDuration * 1000
+          );
+          this.saveAuthData(this.authToken, expirationDate);
           this.router.navigate(['/']);
         }
       });
@@ -61,7 +66,18 @@ export class UserService {
     this.authToken = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
-    this.router.navigate(['/']);
     clearTimeout(this.tokenTimer);
+    this.clearAuthData();
+    this.router.navigate(['/']);
+  }
+
+  private saveAuthData(authToken: string, expirationDate: Date) {
+    localStorage.setItem('token', authToken);
+    localStorage.setItem('expiration', expirationDate.toISOString());
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiration');
   }
 }
