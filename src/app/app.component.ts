bc6995b35dbf1bf20556e7e708f10464278ff8ad
noahlthorne <noahlthorne@gmail.com>
2021-06-16 17:58:36 +0900

    auto authenticate user on app load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/auth/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'swapsies-redux-client';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.autoAuthUser();
+  }
 }
