1a1eac22ed33f86530c5ca94cbdef0af11b143de
noahlthorne <noahlthorne@gmail.com>
2021-06-17 18:10:24 +0900

    add user to listing list component

--- src/app/components/listings/listings-list/listings-list.component.ts
+++ src/app/components/listings/listings-list/listings-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ListingShow } from 'src/app/models/Listing.model';
+import { UserService } from 'src/app/services/auth/user.service';
 import { ListingService } from 'src/app/services/listing/listing.service';
 
 @Component({
@@ -11,15 +12,18 @@ import { ListingService } from 'src/app/services/listing/listing.service';
 })
 export class ListingsListComponent implements OnInit, OnDestroy {
   listings: ListingShow[] = [];
+  userId: string | null;
   private listingsSub: Subscription;
   private gameId: string;
 
   constructor(
     private listingService: ListingService,
+    private userService: UserService,
     public route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
+    this.userId = this.userService.getUserId();
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('gameId')) {
         this.gameId = paramMap.get('gameId')!;
