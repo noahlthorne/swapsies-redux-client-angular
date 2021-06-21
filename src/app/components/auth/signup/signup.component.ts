06b5c5920dd954708ca5e3a8483547896f3e3559
noahlthorne <noahlthorne@gmail.com>
2021-06-21 10:53:32 +0900

    sub to auth status in signup component

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/User.model';
 import { UserService } from 'src/app/services/auth/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
   isLoading: boolean = false;
-  constructor(private userService: UserService) {}
+  private authStatusSub: Subscription;
+  constructor(
+    private userService: UserService,
+    private _snackBar: MatSnackBar
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.authStatusSub = this.userService
+      .getAuthStatusListener()
+      .subscribe((authStatus) => {
+        if (!authStatus) {
+          this.isLoading = false;
+          this.openSnackBar('Invalid email or password');
+        }
+      });
+  }
 
   onSignup(form: NgForm) {
     if (form.invalid) {
@@ -22,5 +37,15 @@ export class SignupComponent implements OnInit {
     const user: User = form.value;
     this.userService.addUser(user);
     form.reset();
+  }
+
+  openSnackBar(message: string) {
+    this._snackBar.open(message, 'dismiss', {
+      panelClass: ['grey-snackbar'],
+    });
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
   }
 }
