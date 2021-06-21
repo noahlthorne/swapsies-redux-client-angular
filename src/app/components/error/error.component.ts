f2ee68a7148864f8fb305ca12167cbdb302d4cf8
noahlthorne <noahlthorne@gmail.com>
2021-06-21 14:02:32 +0900

    inject mat dialog data

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.scss']
+  styleUrls: ['./error.component.scss'],
 })
 export class ErrorComponent implements OnInit {
+  message: string = 'an unknown error occurred!';
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { message: string }) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
