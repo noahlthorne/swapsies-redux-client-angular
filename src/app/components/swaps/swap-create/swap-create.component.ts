6729c99fbebf86d328a1819f7408ed4a106df29e
noahlthorne <noahlthorne@gmail.com>
2021-07-01 10:47:41 +0900

    fetch current user's listings on init

--- src/app/components/swaps/swap-create/swap-create.component.ts
+++ src/app/components/swaps/swap-create/swap-create.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { Listing } from 'src/app/models/Listing.model';
+import { UserService } from 'src/app/services/auth/user.service';
+import { ListingService } from 'src/app/services/listing/listing.service';
 
 @Component({
   selector: 'app-swap-create',
@@ -8,10 +12,36 @@ import { Listing } from 'src/app/models/Listing.model';
 })
 export class SwapCreateComponent implements OnInit {
   @Input() listingRequested: Listing;
+  currentUsersListings: Listing[] = [];
+  currentUserId: string;
+  swapForm: FormGroup;
+  selected: Listing;
 
-  constructor() {}
+  private listingsSub: Subscription;
+
+  constructor(
+    private userService: UserService,
+    private listingService: ListingService
+  ) {}
 
   ngOnInit(): void {
-    console.log('LISTING REQUESTED', this.listingRequested);
+    this.currentUserId = this.userService.getUserId()!;
+    this.listingService.getUserListings(this.currentUserId);
+    this.listingsSub = this.listingService
+      .getListingUpdateListener()
+      .subscribe((listings: Listing[]) => {
+        this.currentUsersListings = listings;
+      });
+    this.swapForm = new FormGroup({
+      listingOffered: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+    });
+  }
+
+  onAddSwap() {
+    if (this.swapForm.invalid) {
+      return;
+    }
   }
 }
