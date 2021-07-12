cd4030d9851c772bb9861a30e22de71869a912da
noahlthorne <noahlthorne@gmail.com>
2021-07-12 14:05:56 +0200

    take swap as input

--- src/app/components/swaps/swap-card/swap-card.component.ts
+++ src/app/components/swaps/swap-card/swap-card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Swap } from 'src/app/models/Swap.model';
 
 @Component({
   selector: 'app-swap-card',
   templateUrl: './swap-card.component.html',
-  styleUrls: ['./swap-card.component.scss']
+  styleUrls: ['./swap-card.component.scss'],
 })
 export class SwapCardComponent implements OnInit {
+  @Input() swap: Swap;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
