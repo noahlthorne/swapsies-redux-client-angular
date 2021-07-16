52b7d1f4afff0f8cf6c0048888e0151a3d6ca759
noahlthorne <noahlthorne@gmail.com>
2021-07-16 13:54:42 +0200

    connect to socketio in header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { UserService } from 'src/app/services/auth/user.service';
+import { environment } from '../../../environments/environment';
+import { SocketioService } from 'src/app/services/socketio/socketio.service';
+
+const SERVER_URL = environment.serverUrl;
 
 @Component({
   selector: 'app-header',
@@ -12,17 +16,25 @@ export class HeaderComponent implements OnInit, OnDestroy {
   userId: string = '';
   private authListenerSub: Subscription;
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private webSocketService: SocketioService
+  ) {}
 
   ngOnInit(): void {
     this.userIsAuthenticated = this.userService.getAuthStatus();
+    console.log(this.userIsAuthenticated);
+    if (this.userIsAuthenticated) {
+      this.userId = this.userService.getUserId()!;
+      this.webSocketService.emit('connect-user', this.userId);
+      this.webSocketService.listen('swap-notification').subscribe((data) => {
+        console.log('DATA', data);
+      });
+    }
     this.authListenerSub = this.userService
       .getAuthStatusListener()
       .subscribe((isAuthenticated) => {
         this.userIsAuthenticated = isAuthenticated;
-        if (this.userIsAuthenticated) {
-          this.userId = this.userService.getUserId()!;
-        }
       });
   }
 
